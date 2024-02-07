5e783ed18abe16a4cc848b0a12f50b297619690a
Leslie <lajor965@learner.dvusd.org>
2024-02-07 08:19:06 -0700

    idk

--- Dex/Dex/Form1.Designer.vb
+++ Dex/Dex/Form1.Designer.vb
@@ -34,20 +34,29 @@ Partial Class Form1
         Me.ShoesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.FirstButton = New System.Windows.Forms.Button()
+        Me.PrevButton = New System.Windows.Forms.Button()
+        Me.NextButton = New System.Windows.Forms.Button()
+        Me.LastButton = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Field1
         '
-        Me.Field1.Location = New System.Drawing.Point(129, 176)
+        Me.Field1.Location = New System.Drawing.Point(136, 130)
         Me.Field1.Name = "Field1"
         Me.Field1.Size = New System.Drawing.Size(100, 22)
         Me.Field1.TabIndex = 0
         '
         'Field2
         '
-        Me.Field2.Location = New System.Drawing.Point(129, 134)
+        Me.Field2.Location = New System.Drawing.Point(136, 172)
         Me.Field2.Name = "Field2"
         Me.Field2.Size = New System.Drawing.Size(100, 22)
         Me.Field2.TabIndex = 1
@@ -55,38 +64,40 @@ Partial Class Form1
         'ToolStrip1
         '
         Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
-        Me.ToolStrip1.Location = New System.Drawing.Point(0, 28)
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 30)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(800, 25)
+        Me.ToolStrip1.Size = New System.Drawing.Size(800, 31)
         Me.ToolStrip1.TabIndex = 2
         Me.ToolStrip1.Text = "ToolStrip1"
         '
         'Field3
         '
-        Me.Field3.Location = New System.Drawing.Point(129, 260)
+        Me.Field3.Location = New System.Drawing.Point(136, 217)
         Me.Field3.Name = "Field3"
         Me.Field3.Size = New System.Drawing.Size(100, 22)
         Me.Field3.TabIndex = 3
         '
         'Field4
         '
-        Me.Field4.Location = New System.Drawing.Point(129, 302)
+        Me.Field4.Location = New System.Drawing.Point(136, 256)
         Me.Field4.Name = "Field4"
         Me.Field4.Size = New System.Drawing.Size(100, 22)
         Me.Field4.TabIndex = 4
         '
         'Field5
         '
-        Me.Field5.Location = New System.Drawing.Point(129, 218)
+        Me.Field5.Location = New System.Drawing.Point(136, 298)
         Me.Field5.Name = "Field5"
         Me.Field5.Size = New System.Drawing.Size(100, 22)
         Me.Field5.TabIndex = 5
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(456, 94)
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(307, 28)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(286, 286)
+        Me.PictureBox1.Size = New System.Drawing.Size(450, 374)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 6
         Me.PictureBox1.TabStop = False
         '
@@ -100,7 +111,7 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShoesToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 30)
         Me.MenuStrip1.TabIndex = 7
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -108,26 +119,125 @@ Partial Class Form1
         '
         Me.ShoesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.SaveToolStripMenuItem})
         Me.ShoesToolStripMenuItem.Name = "ShoesToolStripMenuItem"
-        Me.ShoesToolStripMenuItem.Size = New System.Drawing.Size(51, 24)
+        Me.ShoesToolStripMenuItem.Size = New System.Drawing.Size(51, 26)
         Me.ShoesToolStripMenuItem.Text = "Cats"
         '
         'NewToolStripMenuItem
         '
         Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
-        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
         Me.NewToolStripMenuItem.Text = "New"
         '
         'SaveToolStripMenuItem
         '
         Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
-        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
         Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(73, 133)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 16)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Breed:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(68, 175)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 16)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Height:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.Label3.Location = New System.Drawing.Point(68, 217)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 16)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Weight:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.Label4.Location = New System.Drawing.Point(59, 259)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 16)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Lifespan:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.Label5.Location = New System.Drawing.Point(79, 301)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 16)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Price:"
+        '
+        'FirstButton
+        '
+        Me.FirstButton.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.FirstButton.Location = New System.Drawing.Point(211, 411)
+        Me.FirstButton.Name = "FirstButton"
+        Me.FirstButton.Size = New System.Drawing.Size(90, 35)
+        Me.FirstButton.TabIndex = 13
+        Me.FirstButton.Text = "First"
+        Me.FirstButton.UseVisualStyleBackColor = True
+        '
+        'PrevButton
+        '
+        Me.PrevButton.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.PrevButton.Location = New System.Drawing.Point(307, 411)
+        Me.PrevButton.Name = "PrevButton"
+        Me.PrevButton.Size = New System.Drawing.Size(90, 35)
+        Me.PrevButton.TabIndex = 14
+        Me.PrevButton.Text = "Previous"
+        Me.PrevButton.UseVisualStyleBackColor = True
+        '
+        'NextButton
+        '
+        Me.NextButton.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.NextButton.Location = New System.Drawing.Point(403, 411)
+        Me.NextButton.Name = "NextButton"
+        Me.NextButton.Size = New System.Drawing.Size(90, 35)
+        Me.NextButton.TabIndex = 15
+        Me.NextButton.Text = "Next"
+        Me.NextButton.UseVisualStyleBackColor = True
+        '
+        'LastButton
+        '
+        Me.LastButton.Font = New System.Drawing.Font("Century Gothic", 7.8!, System.Drawing.FontStyle.Bold)
+        Me.LastButton.Location = New System.Drawing.Point(499, 411)
+        Me.LastButton.Name = "LastButton"
+        Me.LastButton.Size = New System.Drawing.Size(90, 35)
+        Me.LastButton.TabIndex = 16
+        Me.LastButton.Text = "Last"
+        Me.LastButton.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LastButton)
+        Me.Controls.Add(Me.NextButton)
+        Me.Controls.Add(Me.PrevButton)
+        Me.Controls.Add(Me.FirstButton)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Field5)
         Me.Controls.Add(Me.Field4)
@@ -159,4 +269,13 @@ Partial Class Form1
     Friend WithEvents ShoesToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents FirstButton As Button
+    Friend WithEvents PrevButton As Button
+    Friend WithEvents NextButton As Button
+    Friend WithEvents LastButton As Button
 End Class
--- Dex/Dex/Form1.vb
+++ Dex/Dex/Form1.vb
@@ -1,7 +1,10 @@
 ﻿Imports System.IO
+Imports System.Web
 
 Public Class Form1
     Dim records(50) As String
+    Dim count As Integer
+    Dim current As Integer
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         Field1.Text = ""
         Field2.Text = ""
@@ -9,22 +12,8 @@ Public Class Form1
         Field4.Text = ""
         Field5.Text = ""
         PictureBox1.Image = Nothing
-    End Sub
-
-    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
-        Dim outFile As New IO.StreamWriter("data.txt")
-        outFile.Write(Field1.Text)
-        outFile.Write("|")
-        outFile.Write(Field2.Text)
-        outFile.Write("|")
-        outFile.Write(Field3.Text)
-        outFile.Write("|")
-        outFile.Write(Field4.Text)
-        outFile.Write("|")
-        outFile.Write(Field5.Text)
-        outFile.Write("|")
-        outFile.WriteLine(PictureBox1.ImageLocation)
-        outFile.Close()
+        current = count
+        count = count + 1
     End Sub
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
@@ -35,12 +24,86 @@ Public Class Form1
         PictureBox1.Load(OpenFileDialog1.FileName)
     End Sub
 
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        SaveToFile()
+    End Sub
+    Sub SaveToFile()
+        Dim r As String
+        r += Field1.Text
+        r += "|"
+        r += Field2.Text
+        r += "|"
+        r += Field3.Text
+        r += "|"
+        r += Field4.Text
+        r += "|"
+        r += Field5.Text
+        r += "|"
+        r += PictureBox1.ImageLocation
+        If count = 0 Then count = 1
+        records(current) = r
+
+        Dim outFile As New StreamWriter("data.txt")
+        For index = 0 To count - 1
+            outFile.WriteLine(records(index))
+        Next
+        outFile.Close()
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If IO.File.Exists("data.txt") Then
-            Dim infile As New StreamReader("data.txt")
-            records(0) = infile.ReadLine
-            'records(1) = infile.ReadLine
-            infile.Close()
+            Dim inFile As New StreamReader("data.txt")
+            While (Not inFile.EndOfStream)
+                records(count) = inFile.ReadLine
+                count = count + 1
+            End While
+            inFile.Close()
+            ShowRecord(0)
+        End If
+    End Sub
+    Public Sub ShowRecord(index As Integer)
+        PictureBox1.Image = Nothing
+        If records(index) <> Nothing Then
+            Dim Fields() As String
+            Fields = records(index).Split("|")
+            Field1.Text = Fields(0)
+            Field2.Text = Fields(1)
+            Field3.Text = Fields(2)
+            Field4.Text = Fields(3)
+            Field5.Text = Fields(4)
+            If File.Exists(Fields(5)) Then
+                PictureBox1.Load(Fields(5))
+            End If
+        End If
+    End Sub
+
+    Private Sub FirstButton_Click(sender As Object, e As EventArgs) Handles FirstButton.Click
+        SaveToFile()
+        current = 0
+        ShowRecord(current)
+    End Sub
+
+    Private Sub PrevButton_Click(sender As Object, e As EventArgs) Handles PrevButton.Click
+        SaveToFile()
+        If current > 0 Then
+            current = current - 1
+        End If
+        ShowRecord(current)
+    End Sub
+
+    Private Sub NextButton_Click(sender As Object, e As EventArgs) Handles NextButton.Click
+        SaveToFile()
+        If current < count - 1 Then
+            current = current + 1
+        End If
+        ShowRecord(current)
+    End Sub
+
+    Private Sub LastButton_Click(sender As Object, e As EventArgs) Handles LastButton.Click
+        SaveToFile()
+        If count > 0 Then
+            current = count - 1
+            ShowRecord(current)
         End If
     End Sub
 End Class
